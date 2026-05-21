b8744e1683fe921f1d8f552b62f5a2462a98c180
DGLee <debilpos@gmail.com>
2026-05-21 12:31:42 +0900

    chore: scaffold Vite + React project

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './styles.css'
+
+createRoot(document.getElementById('root')).render(
+  <h1 style={{ color: '#fff', fontFamily: 'Gaegu, sans-serif' }}>KidsWeb</h1>
+)
